8c3606d1436d306ff63d5010199adc9e19a9f430
Sameena <sameenakp@mapmymarketing.com>
2025-03-24 22:10:02 +0530

    added a warning when a bypass request for the same parent-child-depot-channel has been made multiple times.

---
+++ src/app/layouts/approve-popup/approve-bypass-request/approve-bypass-request.component.ts
@@ -0,0 +1,56 @@
+import {Component, Inject} from '@angular/core';
+import {Router} from "@angular/router";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {SnackBarService} from "../../../services/snack-bar.service";
+
+@Component({
+  selector: 'app-approve-bypass-request',
+  templateUrl: './approve-bypass-request.component.html',
+  styleUrls: ['./approve-bypass-request.component.css']
+})
+export class ApproveBypassRequestComponent {
+
+  remarks: string = '';
+  bypassData: any[] = []
+
+  constructor(private router:Router,
+              private dialog: MatDialog,
+              private toaster: SnackBarService,
+              private dialogRef: MatDialogRef<ApproveBypassRequestComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any)
+  {
+    if (data && data.bypassData && Array.isArray(data.bypassData)) {
+      this.bypassData = data.bypassData
+    }
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  close() {
+    this.dialog.closeAll()
+  }
+
+  ApproveRequest() {
+    if(this.remarks.length !=0){
+      this.dialogRef.close(this.remarks)}
+    else{
+      this.toaster.showError('Please enter remark!')
+    }
+  }
+
+  validateRemarks(event: any) {
+    const regex = /^[A-Za-z0-9\-&(),.% ]*$/;
+    if (!regex.test(event.target.value)) {
+      event.target.value = event.target.value.replace(/[^A-Za-z0-9\-&(),.% ]/g, '');
+    }
+    const input = event.target.value || ''
+    if (input.length > 500) {
+      this.remarks = input.substring(0, 500); // Trim excess if pasted
+    }else {
+      this.remarks = input;
+    }
+  }
+
+}
