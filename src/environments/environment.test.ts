28c5c5a431ba07aec0e3c66cba9d98bb4e666de0
Sameena <sameenakp@mapmymarketing.com>
2025-04-17 16:15:30 +0530

    Changed base URL from HTTP to HTTPS

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   envName: 'test',
-  api: 'http://194.233.76.88:8920/jllappsapi/',
-  login_app_url: 'http://194.233.76.88:8920/',
-  profile_url: 'http://194.233.76.88:8920/#/my-profile',
+  api: 'https://194.233.76.88:8920/jllappsapi/',
+  login_app_url: 'https://194.233.76.88:8920/',
+  profile_url: 'https://194.233.76.88:8920/#/my-profile',
   runMode: 'TEST',
   buildType: ' (TEST BUILD 1.0.01)',
   removeConsolePrints: true
